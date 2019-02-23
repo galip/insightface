ff7e8ff87f34f0269e988eb45cec10b7202c0db0
galip <galippala@gmail.com>
2019-02-24 02:16:04 +0300

    Thesis work
    Thesis work

--- deploy/sample.py
+++ deploy/sample.py
@@ -6,10 +6,12 @@ import numpy as np
 
 import mxnet as mx
 
+import sklearn
+
 parser = argparse.ArgumentParser(description='face model test')
 # general
 parser.add_argument('--image-size', default='112,112', help='')
-parser.add_argument('--model', default='/home/galip/PycharmProjects/insightface/model-r50-am-lfw,0', help='path to load model.')
+parser.add_argument('--model', default='/home/galip/PycharmProjects/insightface/models/model-r50-am-lfw/,0', help='path to load model.')
 parser.add_argument('--ga-model', default='', help='path to load model.')
 parser.add_argument('--gpu', default=0, type=int, help='gpu id')
 parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
@@ -28,7 +30,7 @@ def get_model(ctx, image_size, model_str, layer):
   sym = all_layers[layer+'_output']
   model = mx.mod.Module(symbol=sym, context=ctx, label_names = None)
   #model.bind(data_shapes=[('data', (args.batch_size, 3, image_size[0], image_size[1]))], label_shapes=[('softmax_label', (args.batch_size,))])
-  model.bind(data_shapes=[('data', (1, 3, image_size[0], image_size[1]))])
+  model.bind(data_shapes=[('data', (1, 3, 112, 112))])
   model.set_params(arg_params, aux_params)
   return model
 
@@ -38,17 +40,32 @@ def main():
   if len(args.model) > 0:
       model =get_model(ctx, 112, args.model, 'fc1')
 
-  #aligned face
-  img = cv2.imread('/home/galip/PycharmProjects/insightface/deploy/Tom_Hanks_54745.png')
-  # img = model.get_input(img)
 
-  f1 = model.get_feature(img)
-  print(f1[0:10])
-
-  #aligned face
   img = cv2.imread('/home/galip/PycharmProjects/insightface/deploy/Tom_Hanks_54745.png')
-  f2 = model.get_feature(img)
-  print (f2)
+  #img = model.get_input(img)
+  nimg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  aligned = np.transpose(nimg, (2, 0, 1))
+
+  # I give aligned image directly
+  #f1 = model.get_feature(img)
+
+  input_blob = np.expand_dims(aligned, axis=0)
+  data = mx.nd.array(input_blob)
+  db = mx.io.DataBatch(data=(data,))
+  model.forward(db, is_train=False)
+  embedding = model.get_outputs()[0].asnumpy()
+  embedding = sklearn.preprocessing.normalize(embedding).flatten()
+
+  name = '/home/galip/Desktop/SeNet/GaussianBlur/blurredsource5/repvector5/' + "yessss" + '.txt'
+  file = open(name, 'a')
+  np.savetxt(name, embedding, delimiter=",")
+  file.close()
+
+  print(embedding)
+
+  #img = cv2.imread('/home/galip/PycharmProjects/insightface/deploy/Tom_Hanks_54745.png')
+  #f2 = model.get_feature(img)
+  #print (f2)
 
 if __name__ == '__main__':
     main()
